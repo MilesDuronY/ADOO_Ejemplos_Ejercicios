410dd8340bb8a4f2108b535bd25c5fbfa5b9db26
MilesDuron <milesduron@uaz.edu.mx>
2026-03-10 12:33:23 -0600

    Implementaciones interfaces

--- Interfaces/IDescuento.py
+++ Interfaces/IDescuento.py
@@ -7,9 +7,13 @@ class IDescuento(ABC):
 
 # Extensiones (Nuevas clases, no modificamos las existentes)
 class DescuentoEstudiante(IDescuento):
-    def aplicar(self, monto: float) -> float: return monto * 0.8
-
+    def aplicar(self, monto: float) -> float:return monto * 0.8
+class DescuentoPremium(IDescuento):
+    def aplicar(self, monto: float) -> float: return monto * 0.7
 class Calculadora:
     # Depende de la abstracción, no de la concreción
     def calcular(self, monto: float, descuento: IDescuento) -> float:
         return descuento.aplicar(monto)
+
+calculadora = Calculadora()
+print(calculadora.calcular(200, DescuentoPremium()))
